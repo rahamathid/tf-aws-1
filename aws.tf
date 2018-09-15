9773ccd5ec8ec56c983b658120f85eaf92a2d4f2
rahamathid <rahamathid@yahoo.com>
2018-09-15 20:08:08 +0800

    inti commit

---
+++ aws.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "ap-southeast-1"
+}
+
+resource "aws_instance" "tfvm" {
+  ami = "ami-04336e67b2d52699e"
+  instance_type = "t2.micro"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+access_key = "AKIAJYZPU4IXWXVU37GQ"
+secret_key = "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"
